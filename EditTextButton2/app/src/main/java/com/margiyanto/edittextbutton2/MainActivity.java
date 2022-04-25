3ed8318c1f19fe7a35319e37ba70e6a7b3e8dfc3
margiyanto1023 <104069052+mariyanto1023@users.noreply.github.com>
2022-04-25 13:12:56 +0700

    Add files via upload

---
+++ EditTextButton2/app/src/main/java/com/margiyanto/edittextbutton2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.margiyanto.edittextbutton2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+impo
+import android.widget.Button;
+import amdroid.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnOk = findViewById(R.id.buttonOk);
+        btnOk.setOnClickListener(new view.onclick);
+    }
+    })
+}
